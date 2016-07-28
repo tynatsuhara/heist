731e5628910b9e5781cf12bec83778e334cc8290
Tyler Bonnell <tbonnell@uw.edu>
2016-07-27 22:31:43 -0500

    added alert function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,13 +12,19 @@ public class GameManager : MonoBehaviour {
 	public bool objectivesComplete;
 
 	private List<PossibleObjective> objectives;
+	private List<Character> characters;
+	private PlayerControls player;
 
 	void Start () {
 		instance = this;
 
-		// generate level
+		// 1. generate level
 
+		// 2. spawn characters?
+		characters = Object.FindObjectsOfType<Character>().Where(x => !(x is PlayerControls)).ToList();
+		player = GameObject.FindWithTag("Player");
 
+		// 3. get objectives
 		objectives = Object.FindObjectsOfType<PossibleObjective>().Where(x => x.isObjective && !x.isCompleted).ToList();
 		objectivesComplete = CheckObjectivesComplete();
 	}
@@ -29,6 +35,14 @@ public class GameManager : MonoBehaviour {
 		// WIN!
 		if (!getaway.isEmpty) {
 			Debug.Log("U WIN");
+		}
+	}
+
+	public void AlertInRange(Vector3 location, float range) {
+		foreach (Character c in characters) {
+			if ((c.transform.position - player.transform.position).magnitude < range) {
+				c.Alert();
+			}
 		}
 	}
 
