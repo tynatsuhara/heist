a205e1cf5678910389b73f344f9a0d9e564d10d8
Tyler Bonnell <tbonnell@uw.edu>
2016-09-06 20:12:11 -0500

    more cheat code

--- Assets/Scripts/Cheats.cs
+++ Assets/Scripts/Cheats.cs
@@ -4,28 +4,75 @@ using System.Collections.Generic;
 
 public class Cheats : MonoBehaviour {
 
-	private List<KeyCode> prevKeys;
-
 	// Aliases
 	private static KeyCode up = KeyCode.UpArrow;
 	private static KeyCode down = KeyCode.DownArrow;
 	private static KeyCode left = KeyCode.LeftArrow;
 	private static KeyCode right = KeyCode.RightArrow;
+	private static KeyCode b = KeyCode.B;
+	private static KeyCode a = KeyCode.A;
 
-	private static KeyCode[] trackedKeys = {
-		up, down, left, right
-	};
+	private static KeyCode[] trackedKeys = { up, down, left, right, b, a };
+	public static Cheats instance;
+
+	private List<KeyCode> prevKeys;
+	private Dictionary<string, KeyCode[]> cheats;
+	private Dictionary<string, bool> enabledCheats;
 
 	void Start() {
+		GenerateCheats();
+
 		prevKeys = new List<KeyCode>();
+		instance = this;
+	}
+
+	private void GenerateCheats() {
+		cheats = new Dictionary<string, KeyCode[]>();
+		enabledCheats = new Dictionary<string, bool>();
+
+		// doubles speed of the player
+		cheats.Add("konami", new KeyCode[]{ up, up, down, down, left, right, left, right, b, a });
+
+		foreach (string key in cheats.Keys) {
+			enabledCheats[key] = false;
+		}
 	}
 
 	void Update() {
 		foreach (KeyCode k in trackedKeys) {
 			if (Input.GetKeyDown(k)) {
 				prevKeys.Add(k);
-				Debug.Log(k);
+				CheckAll();
 			}
 		}
+	}
+
+	private void CheckAll() {
+		foreach (string key in cheats.Keys) {
+			KeyCode[] cheatArr = cheats[key];
+			if (cheatArr.Length > prevKeys.Count) {
+				continue;
+			}
+
+			bool match = false;
+			for (int i = 1; i <= cheatArr.Length; i++) {
+				KeyCode cheatKey = cheatArr[cheatArr.Length - i];
+				KeyCode pressedKey = prevKeys[prevKeys.Count - i];
+				if (cheatKey != pressedKey) {
+					break;
+				} else if (i == cheatArr.Length) {
+					match = true;
+				}
+			}
+
+			if (match) {
+				enabledCheats[key] = true;
+				Debug.Log(key + " cheat enabled");
+			}
+		}
+	}
+
+	public bool IsCheatEnabled(string key) {
+		return enabledCheats.ContainsKey(key) && enabledCheats[key];
 	}
 }
