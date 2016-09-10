7d4858ded876f9ca55bc4df0bfb21486c042ec27
Tyler Bonnell <tbonnell@uw.edu>
2016-09-10 18:58:56 -0500

    Modified color shifting

--- Assets/Supafro Splash/ColorShift.cs
+++ Assets/Supafro Splash/ColorShift.cs
@@ -3,33 +3,36 @@ using UnityEngine.UI;
 
 public class ColorShift : MonoBehaviour {
 
+	public RawImage layer1;
+	public RawImage layer2;
 	public float shiftSpeed;
+	public float finalTime;
 	public float fadeAfter;
 	public float fadeRate;
-	private RawImage img;
 	private float timeElapsed;
 	private bool invoked;
 
-	void Start () {
-		img = GetComponent<RawImage>();
-	}
-	
 	void Update () {
-		timeElapsed += Time.deltaTime;
+		Cycle(layer1, 1f);
+		Cycle(layer2, -.5f);
+	}
+
+	private void Cycle(RawImage img, float hueShiftMultiplier) {
 		Color c = img.color;
 		if (c.a <= 0f)
 			return;
 		float h, s, v;
 		Color.RGBToHSV(c, out h, out s, out v);
-		h = (h + shiftSpeed * Time.deltaTime) % 1f;
+		h = (h + hueShiftMultiplier * shiftSpeed * Time.deltaTime) % 1f;
 		Color d = Color.HSVToRGB(h, s, v);
 		if (timeElapsed > fadeAfter)
 			d.a = c.a - fadeRate * Time.deltaTime;
 		if (d.a <= 0 && !invoked) {
-			Invoke("Transition", .2f);
+			Invoke("Transition", finalTime);
 			invoked = true;
 		}
 		img.color = d;
+		timeElapsed += Time.deltaTime;
 	}
 
 	private void Transition() {
