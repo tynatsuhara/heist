0921db41297f18c7f2dd67a77132e4aec888d0f7
Tyler Bonnell <tbonnell@uw.edu>
2016-09-11 18:46:54 -0700

    randomized splash

--- Assets/Supafro Splash/ColorShift.cs
+++ Assets/Supafro Splash/ColorShift.cs
@@ -2,6 +2,7 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+// Class for the intro splash screen
 public class ColorShift : MonoBehaviour {
 
 	public RawImage layer1;
@@ -12,20 +13,27 @@ public class ColorShift : MonoBehaviour {
 	public float fadeRate;
 	private float timeElapsed;
 	private bool invoked;
+	private float layer1Speed;
+	private float layer2Speed;
 
-	void Update () {
-		Cycle(layer1, 1f);
-		Cycle(layer2, -.5f);
+	void Awake() {
+		layer1Speed = Random.Range(.3f, 1.2f) * Random.Range(0, 2) == 0 ? 1f : -1f;
+		layer2Speed = Random.Range(.3f, 1.2f) * Random.Range(0, 2) == 0 ? 1f : -1f;
+		layer1.color = ShiftHue(layer1.color, Random.Range(0f, 1f));
+		layer2.color = ShiftHue(layer2.color, Random.Range(0f, 1f));
+	}
+
+	void Update() {
+		Cycle(layer1, layer1Speed);
+		Cycle(layer2, layer2Speed);
 	}
 
 	private void Cycle(RawImage img, float hueShiftMultiplier) {
 		Color c = img.color;
 		if (c.a <= 0f)
 			return;
-		float h, s, v;
-		Color.RGBToHSV(c, out h, out s, out v);
-		h = (h + hueShiftMultiplier * shiftSpeed * Time.deltaTime) % 1f;
-		Color d = Color.HSVToRGB(h, s, v);
+		
+		Color d = ShiftHue(c, hueShiftMultiplier);
 		if (timeElapsed > fadeAfter)
 			d.a = c.a - fadeRate * Time.deltaTime;
 		if (d.a <= 0 && !invoked) {
@@ -34,6 +42,13 @@ public class ColorShift : MonoBehaviour {
 		}
 		img.color = d;
 		timeElapsed += Time.deltaTime;
+	}
+
+	private Color ShiftHue(Color c, float shiftAmount) {
+		float h, s, v;
+		Color.RGBToHSV(c, out h, out s, out v);
+		h = (h + shiftAmount * shiftSpeed * Time.deltaTime) % 1f;
+		return Color.HSVToRGB(h, s, v);
 	}
 
 	private void Transition() {
