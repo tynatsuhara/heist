69fdac9ad473aa5fdb4364c45b9f7e37f58b2513
Tyler Bonnell <tbonnell@uw.edu>
2016-12-28 11:43:14 -0800

    fixed tint size bug

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public RectTransform tint;
+
+	void Update () {
+		tint.sizeDelta = new Vector2(Screen.width, Screen.height);
+	}
+}
