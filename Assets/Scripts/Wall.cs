69e6964daf2001b41148cf33d18c5c93611e780b
Tyler Bonnell <tbonnell@uw.edu>
2017-01-07 15:13:13 -0800

    update walls to break regardless

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -7,24 +7,23 @@ public class Wall : MonoBehaviour, Damageable {
 	public bool canBeShotThrough;
 	public PicaVoxel.Exploder exploder;
 
+	void Start() {
+		exploder.ValueFilter = canBeShotThrough ? 100 : 0;  // inner wall has val 100, outer has val 0
+	}
+
 	// The return value is used for projectile damage. If the bullet should go
 	// through the object and continue, return true. Otherwise return false.
 	public bool Damage(Vector3 location, Vector3 angle, float damage, bool melee = false, bool playerAttacker = false) {
-		if (damage >= damangeThreshold && canBeShotThrough) {
-			if (exploder != null) {
-				exploder.ExplosionRadius = UnityEngine.Random.Range(.1f, .25f);
-				exploder.transform.position = location + new Vector3(UnityEngine.Random.Range(-.1f, .1f), 
-																	 UnityEngine.Random.Range(-.5f, 1f), 
-																	 UnityEngine.Random.Range(-.1f, .1f));
-				float explosionScale = 3f;
-				exploder.Explode(angle * explosionScale);
-			}
-		} else {
-			// discoloration of Wall?
-			
+		if (damage >= damangeThreshold && exploder != null) {
+			exploder.ExplosionRadius = UnityEngine.Random.Range(.1f, .25f);
+			exploder.transform.position = location + new Vector3(UnityEngine.Random.Range(-.1f, .1f), 
+																	UnityEngine.Random.Range(-.5f, 1f), 
+																	UnityEngine.Random.Range(-.1f, .1f));
+			float explosionScale = 3f;
+			exploder.Explode(angle * explosionScale);
 		}
 
-		return canBeShotThrough;
+		return damage >= damangeThreshold && canBeShotThrough;
 	}
 }
 
