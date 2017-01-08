e5c7a6daa85024bccaf9230f2e11bdad109a4693
Tyler Bonnell <tbonnell@uw.edu>
2017-01-07 19:19:40 -0800

    painting health

--- Assets/Painting.cs
+++ Assets/Painting.cs
@@ -2,10 +2,17 @@
 
 public class Painting : MonoBehaviour, Damageable {
 
+	public float health;
+
 	public bool Damage(Vector3 location, Vector3 angle, float damage, bool melee = false, bool playerAttacker = false) {
-		Rigidbody rb = GetComponent<Rigidbody>();
-		rb.isKinematic = false;
-		enabled = false;
+		if (health == 0) {
+			Rigidbody rb = GetComponent<Rigidbody>();
+			rb.isKinematic = false;
+			enabled = false;
+		} else {
+			transform.RotateAround(transform.position, transform.right, Random.Range(-10, 11));
+			health--;
+		}
 		return true;
 	}
 }
