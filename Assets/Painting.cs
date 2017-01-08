cd3d96fb18385219e749af78b6baccb7d5e753cf
Tyler Bonnell <tbonnell@uw.edu>
2017-01-07 17:53:11 -0800

    paintings

---
+++ Assets/Painting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Painting : MonoBehaviour, Damageable {
+
+	public bool Damage(Vector3 location, Vector3 angle, float damage, bool melee = false, bool playerAttacker = false) {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.isKinematic = false;
+		enabled = false;
+		return true;
+	}
+}
