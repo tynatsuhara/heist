e6d68db16a5a970633138a76049bce5a2b6f9586
Tyler Bonnell <tbonnell@uw.edu>
2017-01-14 20:36:08 -0800

    add menu script and scene

---
+++ Assets/Scripts/CharacterCustomizationMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCustomizationMenu : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	
+	}
+}
