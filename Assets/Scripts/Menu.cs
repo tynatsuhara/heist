4bbfa902410e5c38f6f859d010a786cb09affeec
Tyler Bonnell <tbonnell@uw.edu>
2017-01-20 21:31:03 -0800

    menu stuff

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public RectTransform tint;
+	public Transform cursor;
+
+	void Update () {
+		if (tint != null)
+			tint.sizeDelta = new Vector2(Screen.width, Screen.height);
+	}
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
-	public RectTransform tint;
-
-	void Update () {
-		tint.sizeDelta = new Vector2(Screen.width, Screen.height);
+	void Start() {
+		
 	}
 }
