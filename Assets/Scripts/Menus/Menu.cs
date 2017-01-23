7e7ba840cd62fb9e5fae13b16f2a66c87d2cd5c3
Tyler Bonnell <tbonnell@uw.edu>
2017-01-22 21:50:46 -0800

    actual menu beginning

---
+++ Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public RectTransform tint;
+	public Transform cursor;
+
+	public MenuNode selectedNode;
+	private MenuNode[] all;
+
+	void Start() {
+		all = GetComponentsInChildren<MenuNode>();
+	}
+
+	void Update () {
+		if (tint != null)
+			tint.sizeDelta = new Vector2(Screen.width, Screen.height);
+		GetInput();
+		selectedNode.Select();
+	}
+
+	private void GetInput() {
+		if (selectedNode == null)
+			return;
+
+		selectedNode.Deselect();
+
+		if (Input.GetAxisRaw("Vertical") != 0) {
+			int dir = (int) Mathf.Sign(Input.GetAxisRaw("Vertical"));
+			if (dir == 1 && selectedNode.up != null) {
+				selectedNode = selectedNode.up;
+			} else if (dir == -1 && selectedNode.down != null) {
+				selectedNode = selectedNode.down;
+			}
+		} else if (Input.GetAxisRaw("Horizontal") != 0) {
+			int dir = (int) Mathf.Sign(Input.GetAxisRaw("Horizontal"));
+			if (selectedNode.carousel) {
+				// this has options, cycle and do something based on that
+			} else {
+				// go to the left or right node
+				if (dir == 1 && selectedNode.right != null) {
+					selectedNode = selectedNode.right;
+				} else if (dir == -1 && selectedNode.left != null) {
+					selectedNode = selectedNode.left;
+				}
+			}
+		}
+
+		selectedNode.Select();
+	}
+
+	// public abstract void Carousel(string key, int dir);
+	// public abstract void Enter(string key);	
+}
---
+++ Assets/Scripts/Menus/MenuNode.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNode : MonoBehaviour {
+
+	public string key;
+
+	public MenuNode up;
+	public MenuNode down;
+	public MenuNode left;
+	public MenuNode right;
+
+	public bool carousel;
+	public Material defaultMaterial;
+	public Material selectedMaterial;
+
+	private bool selected;
+
+	public void Select() {
+		SetSelected(true);
+	}
+
+	public void Deselect() {
+		SetSelected(false);
+	}
+
+	private void SetSelected(bool newSelectedState) {
+		if (selected == newSelectedState)
+			return;
+		selected = newSelectedState;
+		Text[] t = GetComponentsInChildren<Text>();
+		foreach (Text txt in t)
+			txt.material = selected ? selectedMaterial : defaultMaterial;
+	}
+}
