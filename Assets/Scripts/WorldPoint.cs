d940f080d923f441bb84b49d8ff55644b9be186f
Tyler Bonnell <tbonnell@uw.edu>
2017-02-19 18:00:44 -0800

    world point class

---
+++ Assets/Scripts/WorldPoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldPoint : MonoBehaviour {
+
+	public float timeToStayHere;
+
+	// If this is true, the thing at this point should face forward in the direction of the blue arrow
+	public bool faceDirection;
+}
